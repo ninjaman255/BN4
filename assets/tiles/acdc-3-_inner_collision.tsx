8b1cfdeba41d4a2262f53ec36800e79c0597d389
Ninjaman255 <aaronclevette@gmail.com>
2025-03-29 18:06:04 -0500

    huge progress thanks to a user named indiana and his tool for inverted polygon collissions.

---
+++ assets/tiles/acdc-3-_inner_collision.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="acdc_2-Collision" tilewidth="684" tileheight="379" tilecount="1" columns="1" objectalignment="top">
+ <image source="acdc-3_inner-mask.png" width="684" height="379"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,234 288,378 291,378 389,329 393,322 402,314 678,177 683,173 683,169 378,16 353,28 297,0 216,41 216,45 218,47 241,58 249,70 248,81 246,85 238,93 108,159 132,172 155,169 177,170 194,173 215,182 226,192 230,201 230,207 226,216 216,226 194,236 169,240 144,239 123,234 107,226 97,216 93,207 93,201 99,189 73,176 3,211 6,215 6,223 0,234"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiles/acdc-3-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="acdc_2-Map" tilewidth="996" tileheight="652" tilecount="0" columns="1" objectalignment="top">
+ <image source="acdc-net-3.png" width="996" height="652"/>
+</tileset>
