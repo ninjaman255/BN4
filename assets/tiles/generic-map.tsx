41a4776746ece8353116a35bf03b07a946297b46
Ninjaman255 <aaronclevette@gmail.com>
2025-04-02 10:55:31 -0500

    Removed unworking acdc-3 net assets and replaced with working ones, created generic-comp and created a backup for easy cloning when making generic comps for BN4.

---
+++ assets/tiles/generic-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="generic-map" tilewidth="640" tileheight="336" tilecount="1" columns="1">
+ <image source="generic-map.png" width="640" height="336"/>
+</tileset>
