3691ef3f780ef052c0a9b1374e516b15322acf4a
Ninjaman255 <aaronclevette@gmail.com>
2025-04-03 13:00:01 -0500

    Town-1 added/fixed up to a clean degree.

---
+++ assets/tiles/town-1-ramp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="town-1-ramp" tilewidth="112" tileheight="104" tilecount="1" columns="1">
+ <image source="town-1-ramp.png" width="112" height="104"/>
+</tileset>
